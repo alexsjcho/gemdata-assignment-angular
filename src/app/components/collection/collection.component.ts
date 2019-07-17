a487abb989894eb116d990d0dbe6e28dbc75080c
Alex Cho <alexsjcho@gmail.com>
2019-07-17 12:37:48 -0700

    successfully rendered collection.title to UI

--- src/app/components/collection/collection.component.ts
+++ src/app/components/collection/collection.component.ts
@@ -8,11 +8,14 @@ import { ApiService } from '../../../services/api.service';
   styleUrls: ['./collection.component.css']
 })
 export class CollectionComponent implements OnInit {
+  collection = {};
+
   constructor(private apiService: ApiService) {}
 
   ngOnInit() {
     this.apiService.getCollection().subscribe((collection) => {
       console.log('Collection:', collection);
+      this.collection = collection;
     });
   }
 }
